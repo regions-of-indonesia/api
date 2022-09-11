4c9e9617c7b5d0fe5d2ea2f537726620fb495e62
flamrdevs <flamrdevs@gmail.com>
2022-09-12 02:00:41 +0700

    hello world

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", async (_request, _reply) => {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  try {
+    const address = await app.listen({ port: Number(process.env.PORT || 3000) });
+    console.log(String(address));
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
