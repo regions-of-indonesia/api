d898d4fdfb115a67f6d24256dd0b680b6a99e828
flamrdevs <flamrdevs@gmail.com>
2022-09-12 02:25:21 +0700

    Switch host to 0.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,10 @@ app.get("/", async (_request, _reply) => {
 
 const start = async () => {
   try {
-    const address = await app.listen({ port: Number(process.env.PORT || 3000) });
+    const address = await app.listen({
+      port: Number(process.env.PORT || 3000),
+      host: "0.0.0.0",
+    });
     console.log(String(address));
   } catch (err) {
     app.log.error(err);
